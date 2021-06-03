228345e085666e16463baa9d3dfb36549ea7c1da
Ron Netzer <ron@e-square.io>
2021-06-03 13:20:34 +0300

    ci: trigger build

--- packages/bdir/src/index.ts
+++ packages/bdir/src/index.ts
@@ -1,5 +1,6 @@
 export { Language, Position } from './lib/bdir.models';
 export { BDirDirective, mapPositionToDirection } from './lib/bdir.directive';
 export { BDirService } from './lib/bdir.service';
+
 export * from './lib/bdir.tokens';
 export * from './lib/bdir.module';
